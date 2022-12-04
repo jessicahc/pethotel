ad821f0271a5da31477b65bdf21a9acefa9a71d3
simesherbs <119460190+simesherbs@users.noreply.github.com>
2022-12-04 15:16:18 -0500

    Update Owner.java

--- pethotel/src/pethotel/Owner.java
+++ pethotel/src/pethotel/Owner.java
@@ -2,72 +2,49 @@ package pethotel;
 import java.util.ArrayList;
 
 public class Owner {
-	String name;
-	String address;
-	String city;
-	String state;
-	String zipcode;
-	String phone;
-	
-	ArrayList<Animal> pets;
-	
-	public Owner() {
-		pets = new ArrayList<Animal>(1);
-	}
-	
-	public void setName(String name) {
+
+	private String name;
+	private String address;
+	private int phone;
+	private int id;
+
+	public Owner() { }
+	public Owner(String name, String address, int phone, int id) {
+		
 		this.name = name;
+		this.address = address;
+		this.phone = phone;
+		this.id = id;
 	}
-	
-	public void setAddress(String addr) {
-		this.address = addr;
-	}
-	
-	public void setCity(String city) {
-		this.city = city;
-	}
-	
-	public void setState(String state) {
-		this.state = state;
-	}
-	
-	public void setZipcode(String zip) {
-		this.zipcode = zip;
-	}
-	
-	public void setPhone(String sz) {
-		this.phone = sz;
-	}
-	
+
+
+	// getters and setters
+
 	public String getName() {
 		return name;
 	}
-	
+	public void setName(String name) {
+		this.name = name;
+	}
+
 	public String getAddress() {
 		return address;
 	}
-	
-	public String getCity() {
-		return city;
+	public void setAddress(String address) {
+		this.address = address;
 	}
-	
-	public String getState() {
-		return state;
-	}
-	
-	public String getZipcode() {
-		return zipcode;
-	}
-	
-	public String getPhone() {
+
+	public int getPhone() {
 		return phone;
 	}
-	
-	public void addPet(Animal a) {
-		pets.add(a);
+	public void setPhone(int phone) {
+		this.phone = phone;
 	}
-	
-	public ArrayList<Animal> getPets() {
-		return pets;
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
 	}
 }
